b50f17d8fd6f14ec8a64ca2dd2a67d92f7c0c320
Andrei.Komarovskikh <Andrei.Komarovskikh@ef8d28be-e053-11de-ab17-5d3be3536607>
2009-12-08 19:10:58 +0000

    Имплементация алгоритма Левенштейна для подсчета расстояния между строками

--- trunk/Sources/FB2 Library/src/org/ak2/utils/CompareUtils.java
+++ trunk/Sources/FB2 Library/src/org/ak2/utils/CompareUtils.java
@@ -58,4 +58,42 @@ public final class CompareUtils {
     public static int compare(final long val1, final long val2) {
         return val1 < val2 ? -1 : val1 > val2 ? 1 : 0;
     }
+    
+    /**
+     * Simple implementation of Levenstein distance algorithm.
+     * @param s1 first string to compare
+     * @param s2 second string to compare
+     * @return
+     */
+    public static int levensteinDistance(final String s1, final String s2) {
+    	int m = s1.length(), n = s2.length();
+    	int[] D1 = new int[n + 1];
+    	int[] D2 = new int[n + 1];
+     
+    	for(int i = 0; i <= n; i ++) {
+    		D2[i] = i;
+    	}
+     
+    	for(int i = 1; i <= m; i ++) {
+    		D1 = D2;
+    		D2 = new int[n + 1];
+    		for(int j = 0; j <= n; j ++) {
+    			if(j == 0) D2[j] = i;
+    			else {
+    				int cost = (s1.charAt(i - 1) != s2.charAt(j - 1)) ? 1 : 0;
+    				if(D2[j - 1] < D1[j] && D2[j - 1] < D1[j - 1] + cost) {
+    					D2[j] = D2[j - 1] + 1;
+    				}
+    				else if(D1[j] < D1[j - 1] + cost) {
+    					D2[j] = D1[j] + 1;
+    				}
+    				else {
+    					D2[j] = D1[j - 1] + cost;
+    				}
+    			}
+    		}
+    	}
+    	return D2[n];
+    }
+
 }
