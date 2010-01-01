9a6139a12becaf1dc63b3d95164be51bc87d969d
Alexander.V.Kasatkin <Alexander.V.Kasatkin@ef8d28be-e053-11de-ab17-5d3be3536607>
2010-01-01 16:45:10 +0000

    Folder scanning improved.

--- Sources/FB2 Library/src/org/ak2/utils/files/FolderScanner.java
+++ Sources/FB2 Library/src/org/ak2/utils/files/FolderScanner.java
@@ -6,37 +6,48 @@ import java.io.FileFilter;
 public class FolderScanner {
 
     public static void enumerateWide(final File root, final FileFilter filter, final int depth) {
-        File[] folders = root.listFiles(new FileFilter() {
+        assert root != null;
+        assert depth >= 0;
+
+        final File[] folders = root.listFiles(new FileFilter() {
             @Override
-            public boolean accept(File file) {
+            public boolean accept(final File file) {
                 if (file.isDirectory()) {
-                    return filter.accept(file);
+                    return acceptFile(filter, file);
                 }
                 return false;
             }
         });
-        int newDepth = depth - 1;
+        final int newDepth = depth - 1;
         if (newDepth >= 0) {
-            for (File folder : folders) {
+            for (final File folder : folders) {
                 enumerateWide(folder, filter, newDepth);
             }
         }
     }
 
+    public static void enumerateWide(final File root, final FileFilter worker) {
+        enumerateWide(root, null, worker, Integer.MAX_VALUE);
+    }
+
     public static void enumerateWide(final File root, final FileFilter filter, final FileFilter worker, final int depth) {
+        assert root != null;
+        assert depth >= 0;
+        assert worker != null;
+
         if (filter.accept(root)) {
-            File[] folders = root.listFiles(new FileFilter() {
+            final File[] folders = root.listFiles(new FileFilter() {
                 @Override
-                public boolean accept(File file) {
+                public boolean accept(final File file) {
                     if (file.isDirectory()) {
-                        return filter.accept(file);
+                        return acceptFile(filter, file);
                     }
                     return false;
                 }
             });
-            int newDepth = depth - 1;
+            final int newDepth = depth - 1;
             if (newDepth >= 0) {
-                for (File folder : folders) {
+                for (final File folder : folders) {
                     enumerateWide(folder, filter, worker, newDepth);
                 }
             }
@@ -45,13 +56,16 @@ public class FolderScanner {
     }
 
     public static void enumerateDepth(final File root, final FileFilter filter, final int depth) {
+        assert root != null;
+        assert depth >= 0;
+
         root.listFiles(new FileFilter() {
             @Override
-            public boolean accept(File file) {
+            public boolean accept(final File file) {
                 if (file.isDirectory()) {
-                    boolean accept = filter.accept(file);
+                    final boolean accept = acceptFile(filter, file);
                     if (accept) {
-                        int newDepth = depth - 1;
+                        final int newDepth = depth - 1;
                         if (newDepth >= 0) {
                             enumerateDepth(file, filter, newDepth);
                         }
@@ -60,5 +74,34 @@ public class FolderScanner {
                 return false;
             }
         });
+    }
+
+    public static void enumerateDepth(final File root, final FileFilter filter, final FileFilter worker, final int depth, final int barrier) {
+        assert root != null;
+        assert depth >= 0;
+        assert worker != null;
+
+        root.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(final File file) {
+                if (file.isDirectory()) {
+                    final boolean accept = acceptFile(filter, file);
+                    if (accept) {
+                        if (depth <= barrier) {
+                            worker.accept(file);
+                        }
+                        final int newDepth = depth - 1;
+                        if (newDepth >= 0) {
+                            enumerateDepth(file, filter, worker, newDepth, barrier);
+                        }
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+    static boolean acceptFile(final FileFilter filter, final File file) {
+        return filter != null ? filter.accept(file) : true;
     }
 }
